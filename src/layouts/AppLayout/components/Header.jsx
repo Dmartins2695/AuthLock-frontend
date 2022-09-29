68d7380ffd3468dbf3f03e48bb914952fe1b871a
Dmartins2695 <62670499+Dmartins2695@users.noreply.github.com>
2022-09-29 23:36:15 +0100

    add logout button

--- src/layouts/AppLayout/components/Header.jsx
+++ src/layouts/AppLayout/components/Header.jsx
@@ -2,14 +2,39 @@ import React from 'react'
 import classes from '../AppLayout.module.sass'
 import { DisplayersSection } from './DisplayerSection'
 import Logo from '../../../assets/images/img.png'
+import Button from '@mui/material/Button'
+import { useLogoutMutation } from '../../../features/auth/authApiSlice'
+import { useGetPasswordsMutation } from '../../../features/password/passwordApiSlice'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { logOut } from '../../../features/auth/authSlice'
 
 export const Header = () => {
+  const [logout] = useLogoutMutation()
+  const [getPasswords] = useGetPasswordsMutation()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const signOut = async () => {
+    await logout().then((response) => {
+      if (response.error) {
+        console.error(response.error)
+      } else {
+        dispatch(logOut())
+        localStorage.removeItem('persist')
+        navigate('/auth/login')
+      }
+    })
+  }
+
   return (
     <div className={classes.headerArea}>
       <div className={classes.headerRowWrapper}>
         <div className={classes.headerColumnLogoWrapper}>
           <div className={classes.headerRowInnerWrapper}>
             <img className={classes.headerLogo} src={Logo} alt='logo' />
+            <div className={classes.logoutButton}>
+              <Button variant='contained' onClick={signOut}>Logout</Button>
+            </div>
           </div>
         </div>
         <DisplayersSection />
