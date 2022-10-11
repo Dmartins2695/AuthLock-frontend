87919f50bd68e372aec3e55cb36d94d3a228430b
Dmartins2695 <62670499+Dmartins2695@users.noreply.github.com>
2022-10-11 20:12:35 +0100

    feat: change app font family

--- src/index.js
+++ src/index.js
@@ -8,16 +8,30 @@ import { store } from './app/store'
 import { Provider } from 'react-redux'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { createTheme, ThemeProvider } from '@mui/material'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`,
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500
+  }
+})
+
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </Provider>
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </ThemeProvider>
 )
 
 // If you want to start measuring performance in your app, pass a function
